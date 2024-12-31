bf43e75d5aeb159c7de63021946da33c9d41e376
Biralo <biralonet@gmail.com>
2024-12-31 14:46:59 -0800

    Add ALU.

--- soc.v
+++ soc.v
@@ -11,6 +11,13 @@ module Briskv (
    wire			 clk;
    wire			 reset;
 
+   reg [4:0]		 leds;
+`ifdef BENCH
+   assign LEDS = leds;
+`else
+   assign LEDS = ~leds;
+`endif
+
    Clock #(
 `ifdef BENCH
 	   .BITS(18)
@@ -26,7 +33,7 @@ module Briskv (
    // Setup memory and initial registers
    reg [31:0]		 instr;
    reg [31:0]		 pc;
-   reg [31:0]		 mem [0:8];
+   reg [31:0]		 mem [0:11];
    initial begin
       pc = 0;
       // NOP: add x0, x0, x0
@@ -35,7 +42,7 @@ module Briskv (
       //
       // add x1, x0, x0
       //                    rs2   rs1       rd
-      mem[0] = 32'b0000000_00000_00000_000_00000_0110011;
+      mem[0] = 32'b0000000_00000_00000_000_00001_0110011;
       // addi x1, x1, 1
       //           imm[11:0]    rs1       rd
       mem[1] = 32'b00000000001_00001_000_00001_0010011;
@@ -48,8 +55,26 @@ module Briskv (
       // addi x1, x1, 1
       //           imm[11:0]    rs1       rd
       mem[4] = 32'b00000000001_00001_000_00001_0010011;
+      // add x2, x1, x0
+      //                    rs2   rs1       rd
+      mem[5] = 32'b0000000_00000_00001_000_00010_0110011;
+      // add x3, x1, x2
+      //                    rs2   rs1       rd
+      mem[6] = 32'b0000000_00010_00001_000_00011_0110011;
+      // srli x3, x3, 3
+      //                   shamt  rs1       rd
+      mem[7] = 32'b0000000_00011_00011_101_00011_0010011;
+      // slli x3, x3, 31
+      //                   shamt  rs1       rd
+      mem[8] = 32'b0000000_11111_00011_001_00011_0010011;
+      // srai x3, x3, 5
+      //                   shamt  rs1       rd
+      mem[9] = 32'b0100000_00101_00011_101_00011_0010011;
+      // srli x1, x3, 26
+      //                   shamt  rs1       rd
+      mem[10] = 32'b0000000_11010_00011_101_00011_0010011;
       // ebreak
-      mem[5] = 32'b00000000001_00000_000_00000_1110011;
+      mem[11] = 32'b00000000001_00000_000_00000_1110011;
    end
 
    // Decode instruction
@@ -69,9 +94,8 @@ module Briskv (
    wire [4:0] rs2_id = instr[24:20];
    wire [4:0] rd_id  = instr[11:7];
 
-   wire [2:0] alu_sel      = instr[14:12];
-   wire	      alu_sel_2    = instr[30];
-   wire [6:0] store_imm_up = instr[31:25];
+   wire [2:0] funct3       = instr[14:12];
+   wire [6:0] funct7       = instr[31:25];
 
    wire [31:0] i_imm = {{21{instr[31]}}, instr[30:20]};
    wire [31:0] s_imm = {{21{instr[31]}}, instr[30:25], instr[11:7]};
@@ -83,10 +107,6 @@ module Briskv (
    reg [31:0]		 regs[0:31];
    reg [31:0]		 rs1;
    reg [31:0]		 rs2;
-   wire [31:0]		 write_back_data;
-   wire			 write_back_en;
-   assign write_back_data = 0;
-   assign write_back_en = 0;
 
 `ifdef BENCH
    integer		 i;
@@ -96,6 +116,28 @@ module Briskv (
       end
    end
 `endif
+
+   wire [31:0] alu_in1 = rs1;
+   wire [31:0] alu_in2 = is_alu_reg ? rs2 : i_imm;
+   wire [4:0]  shamt   = is_alu_reg ? rs2[4:0] : instr[24:20];
+   reg [31:0]  alu_out;
+   always @(*) begin
+      case (funct3)
+	3'b000: alu_out = (funct7[5] & instr[5]) ? (alu_in1 - alu_in2) : (alu_in1 + alu_in2);
+	3'b001: alu_out = alu_in1 << shamt;
+	3'b010: alu_out = $signed(alu_in1) < $signed(alu_in2);
+	3'b011: alu_out = alu_in1 < alu_in2;
+	3'b100: alu_out = alu_in1 ^ alu_in2;
+	3'b101: alu_out = funct7[5] ? ($signed(alu_in1) >>> shamt) : ($signed(alu_in1) >> shamt);
+	3'b110: alu_out = alu_in1 | alu_in2;
+	3'b111: alu_out = alu_in1 & alu_in2;
+      endcase
+   end
+
+   wire [31:0]		 write_back_data;
+   wire			 write_back_en;
+   assign write_back_data = alu_out;
+   assign write_back_en = state == EXECUTE && (is_alu_reg || is_alu_imm);
 
    localparam FETCH_INSTR = 0;
    localparam FETCH_REGS  = 1;
@@ -109,6 +151,13 @@ module Briskv (
       end else begin
 	 if (write_back_en && rd_id != 0) begin
 	    regs[rd_id] <= write_back_data;
+
+	    if (rd_id == 1) begin
+	       leds <= write_back_data;
+	    end
+`ifdef BENCH
+	    $display("x%0d <= %b", rd_id, write_back_data);
+`endif
 	 end
 
 	 case (state)
@@ -136,29 +185,26 @@ module Briskv (
    end
 
 `ifdef BENCH
-   assign LEDS = is_system ? 63 : (1 << state);
-`else
-   assign LEDS = is_system ? ~63 : ~(1 << state);
-`endif
-
-
-`ifdef BENCH
    always @(posedge clk) begin
-      $display("pc=%0d", pc);
-      case (1'b1)
-	is_lui:     $display("lui");
-	is_auipc:   $display("auipc");
-	is_jal:     $display("jal");
-	is_jalr:    $display("jalr");
-	is_branch:  $display("branch");
-	is_load:    $display("load");
-	is_store:   $display("store");
-	is_alu_imm: $display("alu_imm rd_id=%d rs1_id=%d imm=%d alu_sel=%0d", rd_id, rs1_id, i_imm, alu_sel);
-	is_alu_reg: $display("alu_reg rd_id=%d rs1_id=%d rs2_id=%d alu_sel=%b", rd_id, rs1_id, rs2_id, alu_sel);
-	is_fence:   $display("fence");
-	is_system:  $display("system");
-      endcase // case (1'b1)
-      $display("");
+      if (state == FETCH_REGS) begin
+	 case (1'b1)
+	   is_lui:     $display("lui");
+	   is_auipc:   $display("auipc");
+	   is_jal:     $display("jal");
+	   is_jalr:    $display("jalr");
+	   is_branch:  $display("branch");
+	   is_load:    $display("load");
+	   is_store:   $display("store");
+	   is_alu_imm: $display("alu_imm rd=%d rs1=%d imm=%0d funct3=%b", rd_id, rs1_id, i_imm, funct3);
+	   is_alu_reg: $display("alu_reg rd=%d rs1=%d rs2=%d funct3=%b", rd_id, rs1_id, rs2_id, funct3);
+	   is_fence:   $display("fence");
+	   is_system:  $display("system");
+	 endcase
+
+	 if (is_system) begin
+	    $finish();
+	 end
+      end
    end
 `endif
 
